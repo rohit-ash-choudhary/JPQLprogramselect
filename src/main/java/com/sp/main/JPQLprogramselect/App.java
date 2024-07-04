01403bfb4767abc9c1956b5b773aef5c66fe8017
rohit-ash-choudhary <Rooh@#2001S>
2024-07-04 20:17:51 +0530

    JPQL select program

---
+++ src/main/java/com/sp/main/JPQLprogramselect/App.java
@@ -0,0 +1,52 @@
+package com.sp.main.JPQLprogramselect;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import com.sp.entity.Employee;
+
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        EntityManagerFactory entitymanagerfact=Persistence.createEntityManagerFactory("main-persistence");
+         EntityManager em= entitymanagerfact.createEntityManager();
+         
+        //select employee id form the 
+         try {
+         String jql_query="Select e from Employee e";
+        Query query= em.createQuery(jql_query);
+        
+         List<Employee> list_emp= query.getResultList();
+         
+         
+         for(Employee emp:list_emp)
+         {
+        	 System.out.println("ID  "+ emp.getEmployee_id());
+        	 System.out.println("Firstname  "+ emp.getEmployee_firstname());
+        	 System.out.println("Last Name  "+ emp.getLast_firstname());
+        	 System.out.println("Address "+ emp.getAdress());
+        	 System.out.println("Pincode "+ emp.getPincode());
+        	 System.out.println("------------------------------------------");
+         }
+        
+         
+        		 
+         }
+         catch(Exception e)
+         {
+        	 e.printStackTrace();
+         }
+         finally {
+        	 em.close();
+         }
+    }
+}
